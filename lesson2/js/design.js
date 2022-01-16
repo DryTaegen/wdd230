8cb278dbeb7e656227e187fc855bddea81882b76
DryTaegen <73324097+DryTaegen@users.noreply.github.com>
2022-01-15 17:36:22 -0700

    second assignment

---
+++ lesson2/js/design.js
@@ -0,0 +1,4 @@
+const currentDate = new Date();
+document.querySelector("#currentYear").textContent = currentDate.getFullYear();
+
+document.getElementById("lastTouch").textContent = document.lastModified;
