70351820db4583870cdc71c0b5fa2b957498bd3b
DryTaegen <73324097+DryTaegen@users.noreply.github.com>
2022-01-29 14:30:01 -0700

    Co-authored-by: Logan Stucki <loganstucki@gmail.com>

--- chamber/js/index.js
+++ chamber/js/index.js
@@ -4,3 +4,36 @@ document.querySelector("#headerDate").textContent = new Intl.DateTimeFormat("en-
 document.querySelector("#copyWright").textContent = currentDate.getFullYear();
 
 document.getElementById("lastTouch").textContent = document.lastModified;
+
+
+// const classToggle = document.getElementsByClassName("smallMenu");
+// const navBar = document.getElementsByClassName("navbar");
+
+// classToggle.addEventListener("click", () => {navBar.classList.toggle("extended")}, false)
+
+// window.onresize = () => {if (window.innerWidth > 760) navBar.classList.remove("extended")};
+
+
+function toggleMenu(){
+    document.getElementById("navbar").classList.toggle("extendedBar");
+    document.getElementById("smallMenu").classList.toggle("extend");
+}
+const toggle = document.getElementById("smallMenu");
+
+
+function setSmall(){
+    document.getElementById("navbar").classList.remove("extendedBar");
+    document.getElementById("smallMenu").classList.remove("extend");
+}
+
+function setWide(){
+    document.getElementById("navbar").classList.add("extendedBar");
+    document.getElementById("smallMenu").classList.add("extend");
+}
+toggle.onclick = toggleMenu;
+
+window.onresize = () => {
+    if (window.innerWidth > 760) setWide();
+    else setSmall();
+}
+
