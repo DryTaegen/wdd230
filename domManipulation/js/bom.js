d9ca6880ca8c076ad8128b2e3b664f42c6e03d22
DryTaegen <73324097+DryTaegen@users.noreply.github.com>
2022-02-03 22:32:23 -0700

    small changes

--- domManipulation/js/bom.js
+++ domManipulation/js/bom.js
@@ -6,17 +6,20 @@ const list = document.getElementById("list");
 
 
 button.addEventListener("click", function(){
-    newItem = document.createElement("li");
-    delButton = document.createElement("button");
-    delButton.textContent = "❌";
-    delButton.className = "delButton";
-    newItem.innerHTML = input.value;
-    delButton.addEventListener("click", function(){
-        delButton.parentNode.remove();
+    let userInput = input.value;
+    if (!userInput == "") {
+        newItem = document.createElement("li");
+        delButton = document.createElement("button");
+        delButton.textContent = "❌";
+        delButton.className = "delButton";
+        newItem.innerHTML = input.value;
+        delButton.addEventListener("click", function(){
+            delButton.parentNode.remove();
+            input.focus();
+        });
+        newItem.appendChild(delButton);
+        list.appendChild(newItem);
+        input.value = "";
         input.focus();
-    });
-    newItem.appendChild(delButton);
-    list.appendChild(newItem);
-    input.value = "";
-    input.focus();
+    }
 });
