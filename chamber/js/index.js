bcdbe8124f8c6ab384cf5068b8e3032cb2b115e8
DryTaegen <73324097+DryTaegen@users.noreply.github.com>
2022-02-12 17:01:31 -0700

    windchill assingment

--- chamber/js/index.js
+++ chamber/js/index.js
@@ -1,7 +1,7 @@
 const currentDate = new Date();
 document.querySelector("#headerDate").textContent = new Intl.DateTimeFormat("en-UK", { dateStyle: "full"}).format(currentDate);
 
-document.querySelector("#copyWright").textContent = currentDate.getFullYear();
+document.querySelectorAll(".copyWright").textContent = currentDate.getFullYear();
 
 document.getElementById("lastTouch").textContent = document.lastModified;
 
---
+++ chamber/js/windchill.js
@@ -0,0 +1,22 @@
+const windChill = document.querySelector("#windChill")
+var windPut = document.querySelector("#windPut")
+const windspeed = document.querySelector("#windSpeed")
+var tempPut = document.querySelector("#tempPut")
+const submit = document.querySelector("#windSubmit")
+const temperature = document.querySelector("#temp")
+
+
+submit.onclick = windChange
+
+function windChange(){
+    speed = windPut.value
+    temp = tempPut.value
+    console.log(speed, temp)
+    if (speed > 3 && temp <= 50) {
+        farenheit= 35.74 +0.6215 / temp-35.75 / Math.pow(speed, 0.16)+0.4275 / temp / Math.pow(speed, 0.16)
+        windChill.innerHTML = Math.round(farenheit)
+
+    }
+    temperature.innerHTML = temp
+    windspeed.innerHTML = speed
+}
