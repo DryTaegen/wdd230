cfcce6f5b4abf524cfb0018fd0422b85e046d07f
DryTaegen <73324097+DryTaegen@users.noreply.github.com>
2022-03-03 17:44:54 -0700

    upload of chamber json

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -16,36 +16,19 @@ fetch(requestURL)
 
 function displayProphets(prophet) {
     // Create elements to add to the document
-    let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-    let portrait = document.createElement('img');
-    let term = document.createElement("p");
-    let birth = document.createElement("p");
-    let bPlace = document.createElement("P");
+    let card = document.createElement("div");
+    let logo = document.createElement("img");
+    let address = document.createElement("p");
+    let phone = document.createElement("p");
+    let website = document.createElement("a");
   
     // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = `${prophet.name} ${prophet.lastname}`;
     birth.textContent = `Date of birth: ${prophet.birthdate}`;
     bPlace.textContent = `Place of birth: ${prophet.birthplace}`;
-    if (prophet.death == null) {
-        term.textContent = `Is currently serving and has served for ${prophet.length} years.`
 
-    }else {
-    term.textContent = `Served for ${prophet.length} year(s) until their death: ${prophet.death}.`
-    }
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     portrait.setAttribute('src', prophet.imageurl);
-    let suffix;
-    if (prophet.order == 1) {
-        suffix = "st";
-    }else if (prophet.order == 2) {
-        suffix = "nd";
-    }else if (prophet.order == 3) {
-        suffix = "rd";
-    }else {
-        suffix = "th";
-    }
-    portrait.setAttribute('alt',`Portait of ${prophet.name} ${prophet.lastname} - ${prophet.order}${suffix} Latter-day President`);
+    portrait.setAttribute('alt',``);
     portrait.setAttribute('loading', 'lazy');
   
     // Add/append the section(card) with the h2 element
